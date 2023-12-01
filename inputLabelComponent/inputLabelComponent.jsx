c108d1ecc128bf41b3ed42d5ff3ccc12b4d87319
melocotoneroh <aamarber@gmail.com>
2023-12-01 12:11:41 +0100

    React phase one Static texts

---
+++ inputLabelComponent/inputLabelComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function InputLabelComponent(){
+    return (
+        <>
+          <label>HOLA</label>
+          <input type="text"/>
+        </>
+      );
+}
+
+export default InputLabelComponent;
---
+++ main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import InputLabel from './inputLabelComponent/inputLabelComponent'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <InputLabel></InputLabel>
+    <InputLabel></InputLabel>
+    <InputLabel></InputLabel>
+  </React.StrictMode>,
+)
