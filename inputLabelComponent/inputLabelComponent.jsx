70b98d52c58d74898e339fa9099c65b24f8c33e8
Aarón Martín <aamarber@gmail.com>
2023-12-12 16:57:26 +0100

    Added is required chance and message

--- inputLabelComponent/inputLabelComponent.jsx
+++ inputLabelComponent/inputLabelComponent.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function InputLabelComponent({label, placeholder}){
+function InputLabelComponent({label, placeholder, required}){
+
+  const [showRequiredMessage, setShowRequiredMessage] = useState(false);
+
+  const onInputChanges = (event) => {
+    if(!event?.target?.value){
+      setShowRequiredMessage(true);
+    }
+    else if(showRequiredMessage){
+      setShowRequiredMessage(false);
+    }
+  }
+
     return (
         <>
-          <label>{label}</label>
-          <input type="text" placeholder={placeholder}/>
+          <label>{label}
+            <input type="text" placeholder={placeholder} required={required ? 'required' : ''} onChange={onInputChanges}/>
+          </label>
+          {showRequiredMessage ? (<span className='required-error'>The {label} field is required.</span>) : ''}
         </>
       );
 }
--- main.jsx
+++ main.jsx
@@ -5,7 +5,7 @@ import InputLabel from './inputLabelComponent/inputLabelComponent'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <InputLabel label='Name:' placeholder='Peter'></InputLabel>
-    <InputLabel label='Surname:' placeholder='Pearson'></InputLabel>
+    <InputLabel label='Surname:' placeholder='Pearson' required={true}></InputLabel>
     <InputLabel label='Pets name:' placeholder='Marramiau'></InputLabel>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
